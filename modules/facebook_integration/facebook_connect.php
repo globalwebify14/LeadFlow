b68fcd2b054a48f042a1396fff418c7a96ed6f8b
Kundan Mehta <kundanmehta2501@gmail.com>
2026-03-23 16:44:07 +0530

    meta permission fix

--- modules/facebook_integration/facebook_connect.php
+++ modules/facebook_integration/facebook_connect.php
@@ -19,12 +19,13 @@ $_SESSION['fb_oauth_state'] = $stateStr;
 // The Callback URI must match EXACTLY what is saved in the Facebook Developer Portal App settings.
 $redirectUri = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . BASE_URL . 'modules/facebook_integration/facebook_callback.php';
 
-$configId = '1983815745501889';
+$permissions = ['pages_show_list', 'pages_read_engagement', 'leads_retrieval', 'pages_manage_metadata'];
+$scope = implode(',', $permissions);
 
 $loginUrl = "https://www.facebook.com/v19.0/dialog/oauth" . 
     "?client_id=" . urlencode($appId) . 
     "&redirect_uri=" . urlencode($redirectUri) . 
-    "&config_id=" . urlencode($configId) . 
+    "&scope=" . urlencode($scope) . 
     "&state=" . urlencode($stateStr) .
     "&auth_type=rerequest";
 
