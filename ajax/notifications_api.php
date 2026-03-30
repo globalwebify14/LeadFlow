576644962c8c5634cc0556d3be50aa0041ac381e
Kundan Mehta <kundanmehta2501@gmail.com>
2026-03-30 13:37:19 +0530

    fix auto typo

--- ajax/notifications_api.php
+++ ajax/notifications_api.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../config/db.php';
-require_once __DIR__ . '/../includes/auth.php';
+require_once __DIR__ . '/../config/auth.php';
 require_once __DIR__ . '/../models/Notification.php';
 
 if (!isLoggedIn()) {
